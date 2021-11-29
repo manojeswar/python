51c79d9ae5265f419703b89c76141684d6baeecc
manojeswar <manojeswar@gmail.com>
2021-11-29 20:30:37 +0530

    Update move_file.py

--- move_file.py
+++ move_file.py
@@ -1,18 +1,15 @@
-# Python code to search .mp3 files in current
+# Python code to search files in current
 # folder (We can change file type/name and path
 # according to the requirements.
 import shutil, os
   
 # This is to get the directory that the program 
 # is currently running in.
-moveto = "/home/local/GXGROUP/manoj/Downloads/HDD_manoj/moved/"
+moveto = "/home/manoj/Downloads/HDD_manoj/moved/"
 dir_path = os.path.dirname(os.path.realpath(__file__))
   
 for root, dirs, files in os.walk(dir_path):
-    for file in files: 
-  
-        # change the extension from '.mp3' to 
-        # the one of your choice.
+    for file in files:
         if file.endswith('sss.txt'):
 			dst=moveto+file
 			shutil.move(file,dst)
